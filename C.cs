b43fbb49632cca1144aedff4e54dfba83a6fa2e2
Kritartha1 <68478501+Kritartha1@users.noreply.github.com>
2020-07-28 20:33:01 +0530

    Add files via upload

---
+++ C.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using UnityEngine;
+
+
+public class C : MonoBehaviour
+{
+    public float movespeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        movespeed = 4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Translate(movespeed * Input.GetAxis("Horizontal") * Time.deltaTime,0f,movespeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
